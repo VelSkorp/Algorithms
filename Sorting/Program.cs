825cb8d194b397c672c286da525195a586ebcd4c
Влад Концевич <kontsevichv@mail.ru>
2020-07-19 09:54:21 +0300

    added shell sort

---
+++ Sorting/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Sorting
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			int[] v = { 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+
+			ShellSort.Sort(v, v.Length);
+			
+			for (var i = 0; i < v.Length; i++)
+			{
+				Console.WriteLine(v[i]);
+			}
+		}
+	}
+}
